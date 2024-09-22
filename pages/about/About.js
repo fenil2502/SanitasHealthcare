adf47c42aa4f16e6950b146d2fde98a87d368781
Viren602 <viren602@users.noreply.github.com>
2024-09-22 13:20:50 +0530

    Team responsive

--- pages/about/About.js
+++ pages/about/About.js
@@ -7,72 +7,97 @@ class About extends Component {
   render() {
     return (
       <div className="aboutpage">
-        <div className="container">
-          <div className="about-inner">
-            <div className="zigzag">
-              <div className="zigzag-img">
-                <img src="dummy.svg" alt="Dummy" />
-              </div>
-              <div className="zigzag-text">
-                <h2>
-                  Sanitas Healthcare
-                  <br />
-                  Where healthy life is created
-                </h2>
-                <p>
-                  We at Sanitas Healthcare work at a state of the art
-                  manufacturing facility in Gujarat with large scale capacity
-                  for wide range of nutraceuticals products.
-                </p>
-                <p>
-                  WHO–GMP Certified Company - Sanitas Healthcare is occupied in
-                  the manufacturing of different range of Nutraceuticals
-                  products (Tablets, Capsules, Powders, Syrup etc.)
-                </p>
-                <p>
-                  We at Sanitas Healthcare work at a state of the art
-                  manufacturing facility in Gujarat with large scale capacity
-                  for wide range of Nutraceuticals products.
-                </p>
-                <div className="our-work">
-                    <p><FontAwesomeIcon icon={faCheck} />State of the art manufacturing unit</p>
-                    <p><FontAwesomeIcon icon={faCheck} />In house F&D facility</p>
-                    <p><FontAwesomeIcon icon={faCheck} />All dosage form at one place</p>
+        <div className="about-inner">
+          <div className="zigzag">
+            <div className="container">
+              <div className="zigzag-inner">
+                <div className="zigzag-img">
+                  <img src="dummy.svg" alt="Dummy" />
                 </div>
-                <button className="prm-btn">Connect with us</button>
+                <div className="zigzag-text">
+                  <h2>
+                    Sanitas Healthcare
+                    <br />
+                    Where healthy life is created
+                  </h2>
+                  <p>
+                    We at Sanitas Healthcare work at a state of the art
+                    manufacturing facility in Gujarat with large scale capacity
+                    for wide range of nutraceuticals products.
+                  </p>
+                  <p>
+                    WHO–GMP Certified Company - Sanitas Healthcare is occupied
+                    in the manufacturing of different range of Nutraceuticals
+                    products (Tablets, Capsules, Powders, Syrup etc.)
+                  </p>
+                  <p>
+                    We at Sanitas Healthcare work at a state of the art
+                    manufacturing facility in Gujarat with large scale capacity
+                    for wide range of Nutraceuticals products.
+                  </p>
+                  <div className="our-work">
+                    <p>
+                      <FontAwesomeIcon icon={faCheck} />
+                      State of the art manufacturing unit
+                    </p>
+                    <p>
+                      <FontAwesomeIcon icon={faCheck} />
+                      In house F&D facility
+                    </p>
+                    <p>
+                      <FontAwesomeIcon icon={faCheck} />
+                      All dosage form at one place
+                    </p>
+                  </div>
+                  <button className="prm-btn">Connect with us</button>
+                </div>
               </div>
             </div>
-            <div className="team">
+          </div>
+          <div className="team">
+            <div className="container">
               <h2>Our team</h2>
               <div className="team-inner">
                 <div className="team-card">
                   <div className="team-img">
-                    <img src="Devendra-Patel.jpg" alt="Devendra Patel"/>
+                    <img src="Devendra-Patel.jpg" alt="Devendra Patel" />
                   </div>
                   <div className="team-text">
                     <h3>Devendra Patel</h3>
                     <span>Co-owner</span>
-                    <p>Bachlore in PharmacyManager, Formulation & Development</p>
+                    <p>
+                      Bachlore in PharmacyManager,
+                      <br />
+                      Formulation & Development
+                    </p>
                   </div>
                 </div>
                 <div className="team-card">
                   <div className="team-img">
-                    <img src="Tapan-Patel.jpg" alt="Tapan Patel"/>
+                    <img src="Tapan-Patel.jpg" alt="Tapan Patel" />
                   </div>
                   <div className="team-text">
                     <h3>Tapan Patel</h3>
                     <span>Co-owner</span>
-                    <p>Master in PharmacyHOD, Sales</p>
+                    <p>
+                      Master in PharmacyHOD,
+                      <br />
+                      Sales
+                    </p>
                   </div>
                 </div>
                 <div className="team-card">
                   <div className="team-img">
-                    <img src="Vikram-Patel.jpg" alt="Vikram Patel"/>
+                    <img src="Vikram-Patel.jpg" alt="Vikram Patel" />
                   </div>
                   <div className="team-text">
                     <h3>Vikram Patel</h3>
                     <span>Co-owner</span>
-                    <p>Bachlore in PharmacyHOD, Production</p>
+                    <p>
+                      Bachlore in PharmacyHOD,
+                      <br />
+                      Production
+                    </p>
                   </div>
                 </div>
               </div>
