22be26a503daa457140ecd5953ee64e5dcb01283
Fenil Patel <fenil9737@gmail.com>
2024-10-27 19:25:46 +0530

    Font Family Issue_2

--- pages/shared/Layout.js
+++ pages/shared/Layout.js
@@ -13,6 +13,7 @@ const Layout = ({ children, title, searchType }) => {
       <Head>
         <link rel="icon" href="/favicon.png" />
         <link rel="apple-touch-icon" href="/favicon.png" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
         <title>{title} Sanitas Healthcare</title>
       </Head>
       {defaultHeader}
